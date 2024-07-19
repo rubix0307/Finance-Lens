989b1fb8069f3b632e49b06b1bb3839ec9e81714
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-07-19 15:06:42 +0200

    fix translations files

--- main/models.py
+++ main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy
 # Create your models here.
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name=gettext_lazy('Name'))
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=20, decimal_places=5)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255, verbose_name=gettext_lazy('Name'))
+    price = models.DecimalField(max_digits=20, decimal_places=5, verbose_name=gettext_lazy('Price'))
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True, blank=True, verbose_name=gettext_lazy('Category'))
