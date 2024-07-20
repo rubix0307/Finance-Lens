ce79bbfa378ec958b40bd830b103bf82fe993b93
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-07-20 11:54:50 +0200

    edit models

--- main/models.py
+++ main/models.py
@@ -5,13 +5,17 @@ from django.utils.translation import gettext_lazy
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255, verbose_name=gettext_lazy('Name'))
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name=gettext_lazy('Name'))
-    price = models.DecimalField(max_digits=20, decimal_places=5, verbose_name=gettext_lazy('Price'))
+    price = models.DecimalField(max_digits=20, decimal_places=2, verbose_name=gettext_lazy('Price'))
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True, blank=True, verbose_name=gettext_lazy('Category'))
 
     def __init__(self, *args, **kwargs):
-        self.original_name = args[self.get_field_number(self._meta.get_field('name'))]
+        if args:
+            self.original_name = args[self.get_field_number(self._meta.get_field('name'))]
         super(Product, self).__init__(*args, **kwargs)
 
     def get_field_number(self, search_field: str):
