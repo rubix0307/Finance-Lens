6b606a83650848cd6d7ea943ee12b97b12cbac5e
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-07-22 23:51:24 +0200

    add check_webapp_signature

--- utils.py
+++ utils.py
@@ -1,7 +1,36 @@
 import hashlib
 import hmac
+from operator import itemgetter
+from urllib.parse import parse_qsl
 
 
+def check_webapp_signature(token: str, init_data: str) -> bool:
+    """
+    Check incoming WebApp init data signature
 
+    Source: https://core.telegram.org/bots/webapps#validating-data-received-via-the-web-app
 
+    :param token:
+    :param init_data:
+    :return:
+    """
+    try:
+        parsed_data = dict(parse_qsl(init_data))
+    except ValueError:
+        # Init data is not a valid query string
+        return False
+    if "hash" not in parsed_data:
+        # Hash is not present in init data
+        return False
 
+    hash_ = parsed_data.pop('hash')
+    data_check_string = "\n".join(
+        f"{k}={v}" for k, v in sorted(parsed_data.items(), key=itemgetter(0))
+    )
+    secret_key = hmac.new(
+        key=b"WebAppData", msg=token.encode(), digestmod=hashlib.sha256
+    )
+    calculated_hash = hmac.new(
+        key=secret_key.digest(), msg=data_check_string.encode(), digestmod=hashlib.sha256
+    ).hexdigest()
+    return calculated_hash == hash_
