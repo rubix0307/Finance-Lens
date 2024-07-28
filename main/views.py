ce47e7d1550c2c8005f29e2ab1c56c4810b5e2db
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-07-28 14:22:46 +0200

    move monthly_expenses to GET request

--- main/views.py
+++ main/views.py
@@ -13,8 +13,7 @@ def permission_denied_view(request):
 
 @login_required
 def index(request):
-    monthly_expenses = get_monthly_expenses(request.user)
-    context = {'monthly_expenses':monthly_expenses}
+    context = {}
 
     if request.method == 'POST':
 
@@ -28,11 +27,15 @@ def index(request):
             receipt.formset = formset
             return render(request, 'main/receipt/index.html', context={'receipt': receipt, 'is_updated': True})
     else:
+
+        monthly_expenses = get_monthly_expenses(request.user)
         receipts = Receipt.objects.filter(owner=request.user).order_by('-date', '-id').prefetch_related('products')[:100]
 
         for receipt in receipts:
             receipt.formset = ProductFormSet(queryset=receipt.products.all())
+
         context['receipts'] = receipts
+        context['monthly_expenses'] = monthly_expenses
 
     return render(request, 'main/index.html', context=context)
 
