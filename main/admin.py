4b08e9b0dbee44c784f26bb6757a2f8bf6eed32c
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-07-28 18:21:59 +0200

    registration models on admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Product
+
+
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_currency_code', 'price', 'price_usd')
+
+    def get_currency_code(self, obj):
+        return obj.receipt.currency.code
+
+    get_currency_code.short_description = 'Currency Code'
+
+admin.site.register(Product, ProductAdmin)
--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import CustomUser
+
+
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')
+    search_fields = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
