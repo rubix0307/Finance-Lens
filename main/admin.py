4379d9097b60c64a1f4334196480b52926b24f4f
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-08-10 13:42:06 +0200

    added models in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,16 +1,39 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, CurrencyRateHistory, ProductCategory, Receipt
 
 
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'get_currency_code', 'price', 'price_usd')
+    list_display = ('id', 'name', 'get_currency_code', 'price', 'price_usd', 'get_category')
+    list_filter = ('category',)
 
     def get_currency_code(self, obj):
         return obj.receipt.currency.code
 
     get_currency_code.short_description = 'Currency Code'
 
+    def get_category(self, obj):
+        return f'{obj.category.name} {obj.category.id}'
+
+    get_category.short_description = 'Category'
+
+
+class CurrencyRateHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'currency', 'per_usd', 'date',)
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('id', 'shop_name', 'shop_address', 'currency', 'date', 'photo', 'owner', )
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('name',)
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(CurrencyRateHistory, CurrencyRateHistoryAdmin)
+admin.site.register(Receipt, ReceiptAdmin)
+admin.site.register(ProductCategory, ProductCategoryAdmin)
--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.core.cache import cache
 from scraper.currency.scraper import CurrencyScraper
 
+
 class Currency(models.Model):
     code = models.CharField(max_length=3, unique=True)
 
@@ -15,8 +16,9 @@ class CurrencyRateHistory(models.Model):
     date = models.DateField()
 
     class Meta:
-        unique_together = ('currency', 'per_usd', 'date')
+        # unique_together = ('currency', 'per_usd', 'date')
         get_latest_by = ['date']
+
 
 class Receipt(models.Model):
     shop_name = models.CharField(max_length=255, null=True, blank=True)
@@ -25,10 +27,11 @@ class Receipt(models.Model):
     date = models.DateTimeField()
     photo = models.ImageField(upload_to='bot/', null=True, blank=True, max_length=1024)
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
     # TODO Add ForeignKey to family group
 
     def __str__(self):
-        return self.shop_name
+        return f'{self.shop_name}'
 
     class Meta:
         indexes = [
@@ -37,11 +40,13 @@ class Receipt(models.Model):
             models.Index(fields=['owner', 'date'], name='idx_owner_date')
         ]
 
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -68,26 +73,31 @@ class Product(models.Model):
             return cached_rate
 
         rate = None
+        rate_date = None
+        receipt_rate_date = None
+
         try:
             rate = CurrencyRateHistory.objects.filter(currency=self.receipt.currency, date=self.receipt.date).latest()
+
+            rate_date = (rate.date.year, rate.date.month, rate.date.day)
+            receipt_rate_date = (self.receipt.date.year, self.receipt.date.month, self.receipt.date.day)
         except CurrencyRateHistory.DoesNotExist:
             pass
 
-        if not rate or ((rate.date.year, rate.date.month, rate.date.day) != (self.receipt.date.year, self.receipt.date.month, self.receipt.date.day)):
+        if not rate or (rate_date != receipt_rate_date):
             scraper = CurrencyScraper(currency_symbol='USD', date=self.receipt.date)
             scraper.write_rate_history()
 
             rate = CurrencyRateHistory.objects.filter(currency=self.receipt.currency).latest()
 
-        cache.set(cache_key, rate.per_usd, timeout=20) # TODO update timeout
+        cache.set(cache_key, rate.per_usd, timeout=120)  # TODO update timeout
         return rate.per_usd
-
 
     class Meta:
         ordering = ['id']
 
-        indexes = [
-            models.Index(fields=['price'], name='idx_price'),
-            models.Index(fields=['price_usd'], name='idx_price_usd'),
-            models.Index(fields=['category'], name='idx_category'),
-        ]
+        # indexes = [
+        #     models.Index(fields=['price'], name='idx_price'),
+        #     models.Index(fields=['price_usd'], name='idx_price_usd'),
+        #     models.Index(fields=['category'], name='idx_category'),
+        # ]
