7a8f8dcbd9d31ac4c136d8bd3e96e0d66169092b
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-09-07 01:46:32 +0200

    added __str__ for Currency, CurrencyRateHistory

--- main/models.py
+++ main/models.py
@@ -9,11 +9,17 @@ from scraper.currency.scraper import CurrencyScraper
 class Currency(models.Model):
     code = models.CharField(max_length=3, unique=True)
 
+    def __str__(self):
+        return f'<{self.id}>: {self.code}'
+
 
 class CurrencyRateHistory(models.Model):
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     per_usd = models.DecimalField(max_digits=14, decimal_places=7)
     date = models.DateField()
+
+    def __str__(self):
+        return f'{self.id}: {self.per_usd}'
 
     class Meta:
         # unique_together = ('currency', 'per_usd', 'date')
