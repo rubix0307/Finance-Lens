309e9eb422a16b0a50ba871bf7d995c2ffc14dd9
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-09-07 02:05:51 +0200

    added show_feed for a section

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('403/', views.permission_denied_view, name='403'),
     path('', views.index, name='index'),
     path('section/', views.show_section, name='section'),
+    path('feed/', views.show_feed, name='feed'),
     path('delete-receipt/<int:receipt_id>/', views.delete_receipt, name='delete_receipt'),
     path('get-section-stats/', views.get_section_stats, name='get_section_stats'),
 ]
