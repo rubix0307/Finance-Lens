a1294654639c579057128a389c26ed0ece829c41
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-09-07 05:56:24 +0200

    update all query to DB for get_section_statistic

--- main/statistic.py
+++ main/statistic.py
@@ -1,63 +1,48 @@
-import time
 from collections import defaultdict
+from django.db import connection
 
-from django.contrib.auth import get_user_model
-from django.db.models import Sum, F, Q
-from django.db.models.functions import TruncMonth
-
-from main.models import ProductCategory, Receipt
 
 
 def get_section_statistic(section):
-    receipts = Receipt.objects.filter(section=section, date__isnull=False).annotate(
-        base_currency=F('section__currency__code'),
-        base_currency_price=Sum(
-            F('products__prices__price'),
-            filter=Q(products__prices__currency=F('section__currency'))
-        ),
-        month=TruncMonth('date'),
-    ).prefetch_related('products__category', 'currency').values(
-        'base_currency_price',
-        'base_currency',
-        'month',
-        'currency__code',
-        'products__category',
-    ).annotate(
-        currency_sum=Sum('products__price')
-    )
-
     result = defaultdict(
-        lambda: defaultdict(lambda: {'category': None, 'base_currency': None, 'total_base_currency': 0, 'currencies': defaultdict(int)}))
+        lambda: defaultdict(lambda: {'category': None, 'base_currency': None, 'total_base_currency': 0,
+                                     'currencies': defaultdict(int)}))
 
-    categories_cache = {}
+    query = f'''
+    SELECT 
+        to_char(r.date, 'YYYY-MM') AS date,
+        SUM(p.price) AS total_receipt_price,
+        c1.code AS receipt_currency_code,
+        SUM(pp.price) AS total_section_price,
+        c.code AS section_currency_code,
+        pc.name AS product_category_name
+        
+    FROM public.main_section as s
+    
+    LEFT JOIN main_receipt AS r ON r.section_id = s.id
+    LEFT JOIN main_product AS p ON p.receipt_id = r.id
+    LEFT JOIN main_productprice AS pp ON pp.product_id = p.id AND s.currency_id = pp.currency_id
+    LEFT JOIN main_currency AS c ON c.id = pp.currency_id
+    LEFT JOIN main_currency AS c1 ON c1.id = r.currency_id
+    LEFT JOIN main_productcategory AS pc ON pc.id = p.category_id
+    
+    GROUP BY r.currency_id, pp.currency_id, date, pc.name, pc.id, c.code, c1.code
+    
+    ORDER BY date DESC, pc.id DESC;
+    '''
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        columns = [col[0] for col in cursor.description]
+        rows = cursor.fetchall()
+        data = [dict(zip(columns, row)) for row in rows]
 
-    for item in receipts:
-        month = item['month'].strftime('%Y-%m')
-        category_id = item['products__category']
-        currency = item['currency__code']
-        currency_sum = item['currency_sum'] or 0
 
-        if category_id and (category_id not in categories_cache):
-            categories_cache[category_id] = ProductCategory.objects.get(id=category_id)
+    for item in data:
+        category = item['product_category_name']
+        if category:
+            result[item['date']][category]['category'] = category
+            result[item['date']][category]['base_currency'] = item['section_currency_code']
+            result[item['date']][category]['total_base_currency'] += item['total_section_price']
+            result[item['date']][category]['currencies'][item['receipt_currency_code']] += item['total_receipt_price']
 
-        if category_id:
-            category_obj = categories_cache[category_id]
-
-            result[month][category_id]['category'] = category_obj
-            result[month][category_id]['base_currency'] = item['base_currency']
-            result[month][category_id]['total_base_currency'] += item['base_currency_price']
-            result[month][category_id]['currencies'][currency] += currency_sum
-
-    answer = []
-    for month, categories in result.items():
-        d = {'month': month}
-        d['categories'] = {
-            data['category'].name: {
-                'base_currency': data['base_currency'],
-                'total_base_currency': data['total_base_currency'],
-                'currencies': data['currencies']
-            } for category_id, data in categories.items()
-        }
-        answer.append(d)
-
-    return answer
+    return [{'month': k, 'categories': v} for k, v in result.items()]
