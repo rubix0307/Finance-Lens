9d5765d537c463a576a3fe8bab35cef01df48e76
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-09-17 11:47:14 +0200

    updated SQL query for getting data for chart

--- main/statistic.py
+++ main/statistic.py
@@ -1,34 +1,72 @@
-from collections import defaultdict
+import json
 from django.db import connection
 
 
 
-def get_section_statistic(section):
-    result = defaultdict(
-        lambda: defaultdict(lambda: {'category': None, 'base_currency': None, 'total_base_currency': 0,
-                                     'currencies': defaultdict(int)}))
+def get_section_statistic(section, currency):
 
     query = f'''
-    SELECT 
-        to_char(r.date, 'YYYY-MM') AS date,
-        SUM(p.price) AS total_receipt_price,
-        c1.code AS receipt_currency_code,
-        SUM(pp.price) AS total_section_price,
-        c.code AS section_currency_code,
-        pc.name AS product_category_name
+        WITH product_prices_with_currency AS (
+        SELECT
+            TO_CHAR(r.date, 'YYYY-MM') AS month,
+            r.currency_id AS currency_id,
+            SUM(p.price) AS receipt_currency_total_price,
+            pc.name AS category_name,
+            ROUND(SUM(crh_last2.per_usd * (p.price / crh_last.per_usd)), 2) AS selected_currency_total_price,
+            c.code AS selected_currency_code
         
-    FROM public.main_section as s
-    
-    LEFT JOIN main_receipt AS r ON r.section_id = s.id
-    LEFT JOIN main_product AS p ON p.receipt_id = r.id
-    LEFT JOIN main_productprice AS pp ON pp.product_id = p.id AND s.currency_id = pp.currency_id
-    LEFT JOIN main_currency AS c ON c.id = pp.currency_id
-    LEFT JOIN main_currency AS c1 ON c1.id = r.currency_id
-    LEFT JOIN main_productcategory AS pc ON pc.id = p.category_id
-    
-    GROUP BY r.currency_id, pp.currency_id, date, pc.name, pc.id, c.code, c1.code
-    
-    ORDER BY date DESC, pc.id DESC;
+        FROM public.main_section as s
+            LEFT JOIN main_receipt AS r ON r.section_id = s.id
+            LEFT JOIN main_product AS p ON p.receipt_id = r.id
+            
+            LEFT JOIN LATERAL (
+                SELECT crh.*
+                FROM main_currencyratehistory AS crh
+                WHERE crh.currency_id = r.currency_id
+                AND crh.date <= CAST(r.date AS DATE)
+                ORDER BY crh.date DESC
+                LIMIT 1
+            ) AS crh_last ON TRUE
+            LEFT JOIN LATERAL (
+                SELECT crh2.*
+                FROM main_currencyratehistory AS crh2
+                WHERE crh2.currency_id = {currency.id}
+                AND crh2.date <= crh_last.date
+                ORDER BY crh2.date DESC
+                LIMIT 1
+            ) AS crh_last2 ON TRUE
+            LEFT JOIN main_productcategory AS pc ON pc.id = category_id
+            LEFT JOIN main_currency AS c ON c.id = {currency.id}
+        
+        WHERE s.id = {section.id}
+        
+        GROUP BY month, r.currency_id, pc.name, c.code
+        ORDER BY month DESC, category_name, selected_currency_total_price DESC
+        )
+        
+        SELECT
+            month,
+            jsonb_object_agg(
+            category_name,
+            jsonb_build_object(
+                'name', category_name,
+                'currencies', agg_currencies,
+                'selected_currency_total_price', selected_currency_total_price
+            )
+            ) AS categories
+        FROM (
+            SELECT
+                month,
+                category_name,
+                jsonb_object_agg(code, receipt_currency_total_price) AS agg_currencies,
+                SUM(selected_currency_total_price) AS selected_currency_total_price
+            FROM product_prices_with_currency AS p
+                LEFT JOIN main_currency AS c ON c.id = p.currency_id
+                GROUP BY month, category_name
+                HAVING category_name IS NOT NULL
+        ) AS subquery
+        GROUP BY month;
+
     '''
     with connection.cursor() as cursor:
         cursor.execute(query)
@@ -36,13 +74,8 @@ def get_section_statistic(section):
         rows = cursor.fetchall()
         data = [dict(zip(columns, row)) for row in rows]
 
+    for d in data:
+        d['categories'] = json.loads(d['categories'])
+        d['base_currency'] = currency.code
 
-    for item in data:
-        category = item['product_category_name']
-        if category:
-            result[item['date']][category]['category'] = category
-            result[item['date']][category]['base_currency'] = item['section_currency_code']
-            result[item['date']][category]['total_base_currency'] += item['total_section_price']
-            result[item['date']][category]['currencies'][item['receipt_currency_code']] += item['total_receipt_price']
-
-    return [{'month': k, 'categories': v} for k, v in result.items()]
+    return data
